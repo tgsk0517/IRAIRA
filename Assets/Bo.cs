202ae56174817764511e92517b29230ec5c06e4c
nemonoa1021 <nemonoa1021@gmail.com>
2021-06-12 10:05:30 +0900

    ブランチ

---
+++ Assets/Bo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //aaaaa
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
